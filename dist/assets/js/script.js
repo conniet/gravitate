9a97eff5f10f0a13b1aa20545f82dd7d4c2254b4
conniet <conniewtan@gmail.com>
2016-06-21 01:53:11 -0700

    Updating js folder

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -17,6 +17,11 @@ $(document).ready(function(){
 		$('#contact-link').click(function() {
 			$('.menu-list').hide();
 		});
+
+		$(window).scroll(function() {
+			$('.scroll').css("opacity", 1 -
+				$(window).scrollTop()/100);
+		});
 	}
 	/*----------------------------------------------
 	  NAVIGATION -- SCROLL TO ELEMENT ON CLICK
@@ -51,61 +56,85 @@ $(document).ready(function(){
 	// CHANGE NAV BACKGROUND ON SCROLL FOR MOBILE
 	$(window).scroll(function() {
 		if($(window).scrollTop() >= 251) {
-			// $('nav').css("background-color", "rgba(246,248,240,.9)");
 			$('nav').css("background-color", "rgba(16,26,30,.9)");
 		} else {
-			// $('nav').css("background-color", "rgba(246,248,240,.6)");
 			$('nav').css("background-color", "rgba(16,26,30,.6)");
 		}
 	});
 	// CHANGE NAV BACKGROUND ON SCROLL FOR TABLET
 	$(window).scroll(function() {
 		if($(window).scrollTop() >= 548) {
-			// $('nav').css("background-color", "rgba(246,248,240,.9)");
 			$('nav').css("background-color", "rgba(16,26,30,.9)");
 		} else {
-			// $('nav').css("background-color", "rgba(246,248,240,.6)");
 			$('nav').css("background-color", "rgba(16,26,30,.6)");
 		}
 	});
 	// CHANGE NAV BACKGROUND ON SCROLL FOR SCREEN MIN-WIDTH 1024px
 	$(window).scroll(function() {
 		if($(window).scrollTop() >= 720) {
-			// $('nav').css("background-color", "rgba(246,248,240,.9)");
 			$('nav').css("background-color", "rgba(16,26,30,.9)");
 		} else {
-			// $('nav').css("background-color", "rgba(246,248,240,.6)");
 			$('nav').css("background-color", "rgba(16,26,30,.6)");
 		}
 	});
 	// CHANGE NAV BACKGROUND ON SCROLL FOR SCREEN MIN-WIDTH 1280px
 	$(window).scroll(function() {
 		if($(window).scrollTop() >= 890) {
-			// $('nav').css("background-color", "rgba(246,248,240,.9)");
 			$('nav').css("background-color", "rgba(16,26,30,.9)");
 		} else {
-			// $('nav').css("background-color", "rgba(246,248,240,.6)");
 			$('nav').css("background-color", "rgba(16,26,30,.6)");
 		}
 	});
 	// CHANGE NAV BACKGROUND ON SCROLL FOR SCREEN MIN-WIDTH 1440px
 	$(window).scroll(function() {
 		if($(window).scrollTop() >= 1000) {
-			// $('nav').css("background-color", "rgba(246,248,240,.9)");
 			$('nav').css("background-color", "rgba(16,26,30,.9)");
 		} else {
-			// $('nav').css("background-color", "rgba(246,248,240,.6)");
 			$('nav').css("background-color", "rgba(16,26,30,.6)");
 		}
 	});
 	// CHANGE NAV BACKGROUND ON SCROLL FOR SCREEN MIN-WIDTH 1920px
 	$(window).scroll(function() {
 		if($(window).scrollTop() >= 1334) {
-			// $('nav').css("background-color", "rgba(246,248,240,.9)");
 			$('nav').css("background-color", "rgba(16,26,30,.9)");
 		} else {
-			// $('nav').css("background-color", "rgba(246,248,240,.6)");
 			$('nav').css("background-color", "rgba(16,26,30,.9)");
 		}
 	});
+	/*----------------------------------------------
+	  SCROLL ANIMATION - FADE IN FROM BOTTOM
+	----------------------------------------------*/
+	var $animation_elements = $('.bounce-up');
+	var $window = $(window);
+
+	function check_if_in_view() {
+	  var window_height = $window.height();
+	  var window_top_position = $window.scrollTop();
+	  var window_bottom_position = (window_top_position + window_height);
+	  $.each($animation_elements, function() {
+	    var $element = $(this);
+	    var element_height = $element.outerHeight();
+	    var element_top_position = $element.offset().top;
+	    var element_bottom_position = (element_top_position + element_height);
+	    //check to see if this current container is within viewport
+	    if ((element_bottom_position >= window_top_position) &&
+	      (element_top_position <= window_bottom_position)) {
+	      $element.addClass('in-view');
+	    }
+	    // else {
+	    //   $element.removeClass('in-view');
+	    // }
+	  });
+	}
+
+	if($(window).width() >= 1200) {
+
+		$('nav').on('click','#about-link', function(){
+			$('.bounce-up').addClass('nobounce');
+			$('.container').addClass('containerbounce');
+		});
+
+		$window.on('scroll resize', check_if_in_view);
+		$window.trigger('scroll');
+	}
 });
